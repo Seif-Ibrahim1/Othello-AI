898d060ed5024e700d3b2ac9447165990445c950
Seif <SE.SeifIbrahim@gmail.com>
2024-05-15 15:37:11 +0300

    Made GUI use the COntroller

--- Controller.py
+++ Controller.py
@@ -12,104 +12,156 @@ class Controller:
         self.is_player_turn = True
         self.difficulty_level = "medium"
         self.move = Move(self.game.board)
+        self.game_over = False
 
-    def color_and_difficulty(self):
-        while True:
-            color_choice = input("Choose Your Color ('B' or 'W'): ").upper()
-            if color_choice in ["B", "W"]:
-                self.current_player = Player(color_choice)
-                if color_choice == "B":
-                    self.computer_player = Computer("W")
-                elif color_choice == "W":
-                    self.computer_player = Computer("B")
-                    self.is_player_turn = False
+    def color_and_difficulty(self, color, difficulty):
+        self.current_player = Player(color)
+        if color == "B":
+            self.computer_player = Computer("W")
+        elif color == "W":
+            self.computer_player = Computer("B")
+            self.is_player_turn = False
 
-                difficulty_choice = input("Choose Your Difficulty Level ('easy' or 'medium' or 'hard'): ").lower()
-                if difficulty_choice in ["easy", "medium", "hard"]:
-                    if difficulty_choice == "hard":
-                        self.computer_player.set_depth(5)
-                    elif difficulty_choice == "medium":
-                        self.computer_player.set_depth(3)
-                    elif difficulty_choice == "easy":
-                        self.computer_player.set_depth(1)
+        if difficulty == "Hard":
+            self.computer_player.set_depth(5)
+        elif difficulty == "Medium":
+            print("medium")
+            self.computer_player.set_depth(3)
+        elif difficulty == "Easy":
+            self.computer_player.set_depth(1)
 
-                    break
-                else:
-                    print("Invalid Difficulty Choice!")
-            else:
-                print("Invalid Color Choice!")
+    # def color_and_difficulty(self):
+    #     while True:
+    #         color_choice = input("Choose Your Color ('B' or 'W'): ").upper()
+    #         if color_choice in ["B", "W"]:
+    #             self.current_player = Player(color_choice)
+    #             if color_choice == "B":
+    #                 self.computer_player = Computer("W")
+    #             elif color_choice == "W":
+    #                 self.computer_player = Computer("B")
+    #                 self.is_player_turn = False
 
-    def get_user_move(self):
-        available_moves = self.move.get_available_moves(self.current_player)
-        if not available_moves:
-            print("You don't have available moves! Skipping your turn!")
-            return None
+    #             difficulty_choice = input("Choose Your Difficulty Level ('easy' or 'medium' or 'hard'): ").lower()
+    #             if difficulty_choice in ["easy", "medium", "hard"]:
+    #                 if difficulty_choice == "hard":
+    #                     self.computer_player.set_depth(5)
+    #                 elif difficulty_choice == "medium":
+    #                     self.computer_player.set_depth(3)
+    #                 elif difficulty_choice == "easy":
+    #                     self.computer_player.set_depth(1)
 
-        while True:
-            try:
-                row = int(input("Enter row (0-7): "))
-                col = int(input("Enter column (0-7): "))
-                if self.move.is_valid_move(row, col, self.current_player):
-                    return row, col
-                else:
-                    print("Invalid move. Try again.")
-            except ValueError:
-                print("Invalid move. Try again.")
+    #                 break
+    #             else:
+    #                 print("Invalid Difficulty Choice!")
+    #         else:
+    #             print("Invalid Color Choice!")
 
-    def play(self):
-        print("Welcome To Othello Game!")
-        # first choose the color and the difficulty level
-        self.color_and_difficulty()
-        print("The Initial Board:")
-        self.game.print_board()
-        # then start the game
-        while not self.game.is_game_over():
-            if self.is_player_turn:
-                print(f"Your Turn :")
-                coords = self.get_user_move()
-                if coords is None:
-                    self.is_player_turn = not self.is_player_turn
-                    continue
+    def make_user_move(self, row, col):
+        if self.move.is_valid_move(row, col, self.current_player):
+            print(f"Player played: {row}, {col}")
+            self.move.move(row, col, self.current_player)
+            self.is_player_turn = False
+            return True
+        return False
+        
+    
+    # def get_user_move(self):
+    #     available_moves = self.move.get_available_moves(self.current_player)
+    #     if not available_moves:
+    #         print("You don't have available moves! Skipping your turn!")
+    #         return None
 
-                row, col = coords
-                self.move.move(row, col, self.current_player)
-            else:
-                print(f"Computer's Turn :")
-                coords = self.computer_player.play(self.game)
-                if coords is None:
-                    print("Computer has no valid moves. Skipping turn.")
-                    self.is_player_turn = not self.is_player_turn
-                    if not self.move.get_available_moves(self.current_player):
-                        break
-                    continue
+    #     while True:
+    #         try:
+    #             row = int(input("Enter row (0-7): "))
+    #             col = int(input("Enter column (0-7): "))
+    #             if self.move.is_valid_move(row, col, self.current_player):
+    #                 return row, col
+    #             else:
+    #                 print("Invalid move. Try again.")
+    #         except ValueError:
+    #             print("Invalid move. Try again.")
 
+    def make_computer_move(self):
+        if not self.game.is_game_over():
+            coords = self.computer_player.play(self.game)
+            if coords is not None:
                 row, col = coords
                 self.move.move(row, col, self.computer_player)
+                self.is_player_turn = True
+                return True
+            else:
+                self.is_player_turn = True
+                return False
+            
+    def switch_to_player(self):
+        if not self.game_over and not self.move.get_available_moves(self.current_player):
+            return False
+        
+        return True
 
-            # Display the updated board
-            print("Updated Board:")
-            self.game.print_board()
-            print("Scores :")
-            score = self.game.get_score()
-            print(f"Player 1 (Black) : {score['B']}")
-            print(f"Player 2 (White) : {score['W']}")
+    def check_game_over(self):
+        if self.game.is_game_over() or (not self.move.get_available_moves(self.current_player) and not self.move.get_available_moves(self.computer_player)):
+            self.game_over = True
+            return True
+        
+        return False
             
 
-            # change turns
-            self.is_player_turn = not self.is_player_turn
+#     def play(self):
+#         print("Welcome To Othello Game!")
+#         # first choose the color and the difficulty level
+#         self.color_and_difficulty()
+#         print("The Initial Board:")
+#         self.game.print_board()
+#         # then start the game
+#         while not self.game.is_game_over():
+#             if self.is_player_turn:
+#                 print(f"Your Turn :")
+#                 coords = self.get_user_move()
+#                 if coords is None:
+#                     self.is_player_turn = not self.is_player_turn
+#                     continue
 
-        # game is over
-        winner = self.game.get_winner()
-        if winner == "Draw":
-            print("It's a Draw!")
-        else:
-            if self.current_player.color == "W" and winner == "W":
-                print("YOU'RE THE WINNER!")
-            elif self.current_player.color == "B" and winner == "B":
-                print("YOU'RE THE WINNER!")
-            else:
-                print("COMPUTER IS THE WINNER!")
+#                 row, col = coords
+#                 self.move.move(row, col, self.current_player)
+#             else:
+#                 print(f"Computer's Turn :")
+#                 coords = self.computer_player.play(self.game)
+#                 if coords is None:
+#                     print("Computer has no valid moves. Skipping turn.")
+#                     self.is_player_turn = not self.is_player_turn
+#                     if not self.move.get_available_moves(self.current_player):
+#                         break
+#                     continue
+
+#                 row, col = coords
+#                 self.move.move(row, col, self.computer_player)
+
+#             # Display the updated board
+#             print("Updated Board:")
+#             self.game.print_board()
+#             print("Scores :")
+#             score = self.game.get_score()
+#             print(f"Player 1 (Black) : {score['B']}")
+#             print(f"Player 2 (White) : {score['W']}")
+            
+
+#             # change turns
+#             self.is_player_turn = not self.is_player_turn
+
+#         # game is over
+#         winner = self.game.get_winner()
+#         if winner == "Draw":
+#             print("It's a Draw!")
+#         else:
+#             if self.current_player.color == "W" and winner == "W":
+#                 print("YOU'RE THE WINNER!")
+#             elif self.current_player.color == "B" and winner == "B":
+#                 print("YOU'RE THE WINNER!")
+#             else:
+#                 print("COMPUTER IS THE WINNER!")
 
 
-controller = Controller()
-controller.play()
+# controller = Controller()
+# controller.play()
--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ from GameRepresentation import GameBoard
 from Player import Player
 from ComputerAI import Computer
 from Move import Move
+from Controller import Controller
 
 class OthelloGUI:
     def __init__(self, master):
@@ -12,12 +13,7 @@ class OthelloGUI:
         master.geometry("680x770")  # Adjusted window size
         master.config(bg="#0e2137")  # Dark blue background color
 
-        self.game = GameBoard()
-        self.current_player = None
-        self.computer_player = None
-        self.is_player_turn = True  # By default, player starts
-        self.move = Move(self.game.board)
-        self.game_over = False
+        self.controller = Controller()
 
         self.color_label = tk.Label(master, text="Choose Your Color:", font=("Helvetica", 24), bg="#0e2137", fg="white")
         self.color_label.grid(row=0, column=0, padx=20, pady=(40, 10), sticky="ew")  # Increased top padding
@@ -62,21 +58,11 @@ class OthelloGUI:
         difficulty_choice = self.difficulty_var.get()
 
         if color_choice == "Black":
-            self.current_player = Player("B")
-            self.computer_player = Computer("W")
+            color_choice = "B"
         else:
-            self.current_player = Player("W")
-            self.computer_player = Computer("B")
+            color_choice = "W"
 
-        if difficulty_choice == "Hard":
-            self.computer_player.set_depth(5)
-        elif difficulty_choice == "Medium":
-            self.computer_player.set_depth(3)
-        elif difficulty_choice == "Easy":
-            self.computer_player.set_depth(1)
-
-        if self.current_player.color == "W":
-            self.is_player_turn = False
+        self.controller.color_and_difficulty(color_choice, difficulty_choice)
 
         # Remove color and difficulty selection widgets
         self.color_label.grid_remove()
@@ -91,8 +77,8 @@ class OthelloGUI:
         self.create_board_buttons()
 
         self.update_board_display()
-        if not self.is_player_turn:
-            self.computer_play()
+        if not self.controller.is_player_turn:
+            self.controller.make_computer_move()
 
     def create_board_buttons(self):
         cell_size = 80  # Adjust the cell size to make the board smaller
@@ -101,50 +87,41 @@ class OthelloGUI:
                 x0, y0 = col * cell_size, row * cell_size
                 x1, y1 = x0 + cell_size, y0 + cell_size
                 self.board_canvas.create_rectangle(x0, y0, x1, y1, outline="black", fill="#0e2137")
-                radius = 30  # Adjust the radius of the circle
                 self.board_buttons[row][col] = self.board_canvas.create_oval(x0 + 5, y0 + 5, x1 - 5, y1 - 5, fill="#0e2137")
                 self.board_canvas.tag_bind(self.board_buttons[row][col], "<Button-1>",
                                         lambda event, row=row, col=col: self.make_move(row, col))
 
     def make_move(self, row, col):
-        if self.is_player_turn:
-            if self.move.is_valid_move(row, col, self.current_player):
-                print(f"Player played: {row}, {col}")
-                self.move.move(row, col, self.current_player)
-                self.is_player_turn = False
-                self.update_board_display()
-                self.check_game_over()
-                if not self.game_over:
-                    self.computer_play()
-            else:
-                self.show_alert("Invalid move. Try again.")
+        if self.controller.make_user_move(row, col):
+            self.update_board_display()
+            self.check_game_over()
+            if not self.controller.game_over:
+                self.computer_play()
+        else:
+            self.show_alert("Invalid move. Try again.")
 
     def computer_play(self):
-        if not self.game.is_game_over():
-            coords = self.computer_player.play(self.game)
-            if coords is not None:
-                row, col = coords
-                self.move.move(row, col, self.computer_player)
-                self.is_player_turn = True
-                self.update_board_display()
-                self.check_game_over()
-                if not self.game_over and not self.move.get_available_moves(self.current_player):
-                    self.show_alert("You have no valid moves. Skipping turn.")
-                    self.computer_play()
-            else:
-                self.show_alert("Computer has no valid moves. Skipping turn.")
-                self.is_player_turn = True
-                self.update_board_display()
-                self.check_game_over()
+        if self.controller.make_computer_move():
+            self.update_board_display()
+            self.check_game_over()
+            if not self.controller.switch_to_player():
+                self.show_alert("You have no valid moves. Skipping turn.")
+                self.computer_play()
+
+        else:
+            self.show_alert("Computer has no valid moves. Skipping turn.")
+            self.update_board_display()
+            self.check_game_over()
+
+        
 
     def check_game_over(self):
-        if self.game.is_game_over() or (not self.move.get_available_moves(self.current_player) and not self.move.get_available_moves(self.computer_player)):
-            self.game_over = True
+        if self.controller.check_game_over():
             self.disable_board_buttons()
-            winner = self.game.get_winner()
+            winner = self.controller.game.get_winner()
             if winner == "Draw":
                 self.show_alert("It's a draw!")
-            elif winner == self.current_player.color:
+            elif winner == self.controller.current_player.color:
                 self.show_alert("YOU'RE THE WINNER!")
             else:
                 self.show_alert("COMPUTER IS THE WINNER!")
@@ -160,15 +137,15 @@ class OthelloGUI:
 
         for row in range(8):
             for col in range(8):
-                if self.game.board[row][col] == "B":
+                if self.controller.game.board[row][col] == "B":
                     self.board_canvas.itemconfig(self.board_buttons[row][col], fill="black")
-                elif self.game.board[row][col] == "W":
+                elif self.controller.game.board[row][col] == "W":
                     self.board_canvas.itemconfig(self.board_buttons[row][col], fill="white")
                 else:
                     self.board_canvas.itemconfig(self.board_buttons[row][col], fill="#0e2137")  # Dark blue for empty cells
 
          # Highlight available moves
-        available_moves = self.move.get_available_moves(self.current_player)
+        available_moves = self.controller.move.get_available_moves(self.controller.current_player)
         for move in available_moves:
             row, col = move
             x, y = col * 80, row * 80  # Adjusted cell size
@@ -180,14 +157,14 @@ class OthelloGUI:
                                                     outline="light gray", width=5)  # Light gray outline
             self.available_moves.append(move_id)
 
-        scores = self.game.get_score()
-        if self.current_player.color == "B":
+        scores = self.controller.game.get_score()
+        if self.controller.current_player.color == "B":
             self.update_info_label("Player's turn", scores)
         else:
             self.update_info_label("Computer's turn", scores)
 
     def update_info_label(self, message, scores):
-        score_text = f"Player: {scores[self.current_player.color]}   Computer: {scores[self.computer_player.color]}"  # Construct score text
+        score_text = f"Player: {scores[self.controller.current_player.color]}   Computer: {scores[self.controller.computer_player.color]}"  # Construct score text
         self.info_label.config(text=message + "\n" + score_text)  # Concatenate with message and set as label text
 
     def show_alert(self, message):
